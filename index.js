78533549a4b088a27d596211e5b8aede30094050
K-levr <rigdaokyle@gmail.com>
2024-03-15 09:37:40 -0700

    done

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log("Hello world");
+let B = false
+    I = false
+    N = false
+    G = false
+    O = false
+
+    function newNumber (maxNumber) {
+        let x = Math.floor(Math.random() * maxNumber) + 1;
+        
+    //if else statement
+            if(x <= 15) {
+                console.log(`New number ${x} belongs to "B"`);
+                B = true
+            } else if (x >= 16 && x <= 30) {
+                console.log(`New Number ${x} belongs to "I"`);
+                I = true
+            } else if (x >= 31 && x <= 45) {
+                console.log(`New Number ${x} belongs to "N "`);
+                N = true
+            } else if (x >= 46 && x <= 68) {
+                console.log(`New Number ${x} belongs to "G "`);
+                G = true
+            } else if (x >= 61 && x <= 75) {
+                console.log(`New Number ${x} belongs to "O "`);
+                O = true
+            } else {
+                console.log(`New Number ${x} is invalid.`);
+            }
+            if (B == true && I == true && N == true && G == true && O == true) {
+                console.log(`BINGOOOOO!!`);
+            }
+            }
+            newNumber(75);
